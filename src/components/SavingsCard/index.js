5799e610352a44d0eb2bdc088b6caffc705e0921
Nicole Roberts <nicolerobertsb@gmail.com>
2019-11-05 12:56:06 -0800

    progressbarcode

--- src/components/SavingsCard/index.js
+++ src/components/SavingsCard/index.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import ProgressBar from 'react-bootstrap/ProgressBar'
 
-export default class SavingsCard extends Component {
-    render() {
-        return (
-            <div className="card mb-5">
-                <div className="card-header">Emergency Fund</div>
-                <div className="progress">
-                    <div className="progress-bar w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-            </div>
-        )
+// export default class SavingsCard extends Component {
+//     render() {
+//         return (
+//             <div className="card mb-5">
+//                 <div className="card-header">Emergency Fund</div>
+//                 <div className="progress">
+//                     <div className="progress-bar w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+function SavingsCard (props) {
+
+    const progressPercent = 50;
+  //   const progressPercent = props.calValue;
+    const progressInstance = <ProgressBar now={progressPercent} label={`${progressPercent}%`} />;
+  
+      return (
+  
+          <Card>
+          <Card.Body>
+            <Card.Title>{props.title}title</Card.Title>
+            <Card.Text>
+              <div >
+                  {progressInstance}
+              </div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      );
     }
-}
+    
+    export default SavingsCard;

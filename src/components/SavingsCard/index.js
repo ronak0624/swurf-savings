cae0a87422a01d656c4815b89bc41ac32cf4afec
Ronak Patel <ronak0624@gmail.com>
2019-11-07 14:18:24 -0800

    savings screen styling

--- src/components/SavingsCard/index.js
+++ src/components/SavingsCard/index.js
@@ -2,29 +2,41 @@ import React, { Component, state } from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import ProgressBar from 'react-bootstrap/ProgressBar'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 import './style.css'
 
-function SavingsCard (props) {
+function SavingsCard(props) {
 
-    const progress = Math.round(props.priceRemaining / props.price * 100);
+  const progress = Math.round(props.priceRemaining / props.price * 100);
 
-    const progressInstance = <ProgressBar now={progress} label={`${progress}%`} />;
-  
-      return (
-        <Card className="mb-5 mx-auto">
-          <Card.Body>
-            <Card.Title>{props.title}</Card.Title>
-            <Card.Text>
-                {progressInstance}
-            </Card.Text>
-            <Card.Text className="text-center">
-                ${props.priceRemaining} | ${props.price}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      );
-    }
-    
+  const progressInstance = <ProgressBar now={progress} label={`${progress}%`} />;
+
+  return (
+    <Card className="mb-5 mx-auto">
+      <Card.Body>
+        <Card.Text className="text-center">I need this</Card.Text>
+        <Card.Title className="mb-4">{props.title}</Card.Title>
+        <Card.Text>
+          {progressInstance}
+        </Card.Text>
+        <Card.Text className="text-center">
+          <Row className="justify-content-md-center savings-info">
+            <Col xs lg="2">
+              ${props.priceRemaining}
+            </Col>
+            <Col className="text-muted" md="auto">|</Col>
+            <Col xs lg="2">
+            ${props.price}
+            </Col>
+          </Row>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
 export default SavingsCard;
 
 
@@ -47,7 +59,7 @@ export default SavingsCard;
 // for(var i = 0; i < 40; i++){
 //   let start = savings[i].price
 //   let end = savings[i].priceRemaining
-  
+
 //   if(start < end){
 //     console.log('swapping');
 //     let swap = savings[i].price;

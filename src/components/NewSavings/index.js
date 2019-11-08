fa2b19b731532c6dbaf8340ae24ad98e982a2ccb
Nicole Roberts <nicolerobertsb@gmail.com>
2019-11-08 12:22:48 -0800

    savings form works

--- src/components/NewSavings/index.js
+++ src/components/NewSavings/index.js
@@ -1,65 +1,126 @@
-import React, { useState } from 'react';
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export default function NewSavings(props) {
+  const [validated, setValidated] = useState(false);
   const [show, setShow] = useState(false);
+
+  const [state, setState] = useState({
+    goal: "",
+    priority: "",
+    price: ""
+  });
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  // handleInputChange = event => {
-  //   const name =event.target.name;
-  //   const value = event.target.value;
-  //   this.setState({
-  //     [name]:value
-  //   })
-  // }
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setState(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
 
-  // handleSubmit = event => {
-  //   handleClose();
-  //   const form = event.currentTarget;
-  //   if (form.checkValidity() === false) {
-  //     event.preventDefault();
-  //     event.stopPropagation();
-  //   }
-
-  //   setValidated(true);
-  // };
+  const handleSubmit = event => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+    if (validated === true) {
+      handleClose();
+    }
+    console.log(state);
+  };
 
   return (
     <div>
       <div className="row mb-4">
-        <button onClick={handleShow} type="button" className="btn btn-outline-primary mx-auto w-50">+</button>
+        <button
+          onClick={handleShow}
+          type="button"
+          className="add-button mx-auto"
+        >
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="far"
+            data-icon="plus"
+            class="svg-inline--fa fa-plus fa-w-12"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+          >
+            <path
+              fill="currentColor"
+              d="M368 224H224V80c0-8.84-7.16-16-16-16h-32c-8.84 0-16 7.16-16 16v144H16c-8.84 0-16 7.16-16 16v32c0 8.84 7.16 16 16 16h144v144c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V288h144c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16z"
+            ></path>
+          </svg>
+        </button>
       </div>
 
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
           <Modal.Title>Add new {props.title}</Modal.Title>
         </Modal.Header>
+
         <Modal.Body>
-          <Form>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Form.Group controlId="exampleForm.ControlInput1">
               <Form.Label>Goal</Form.Label>
-              <Form.Control type="email" placeholder="New Skis" />
+              <Form.Control
+                name="goal"
+                onChange={handleChange}
+                value={state.goal}
+                type="text"
+                placeholder="New Skis"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a name for your goal.
+              </Form.Control.Feedback>
             </Form.Group>
+
             <Form.Group controlId="exampleForm.ControlSelect1">
               <Form.Label>Importance</Form.Label>
-              <Form.Control as="select">
+              <Form.Control
+                name="priority"
+                onChange={handleChange}
+                value={state.priority}
+                as="select"
+              >
                 <option>1 (I need this as fast as possible)</option>
                 <option>2 (I really want this)</option>
                 <option>3 (I want this but don't need it right away)</option>
               </Form.Control>
             </Form.Group>
             <Form.Group controlId="exampleForm.ControlInput1">
-              <Form.Label>Pricel</Form.Label>
-              <Form.Control type="number" placeholder="$400" />
+              <Form.Label>Price ($)</Form.Label>
+              <Form.Control
+                onChange={handleChange}
+                value={state.price}
+                name="price"
+                type="number"
+                placeholder="$0"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid price
+              </Form.Control.Feedback>
             </Form.Group>
           </Form>
         </Modal.Body>
+
         <Modal.Footer>
-          <Button variant="primary">
+          <Button
+            className="submit-btn"
+            variant="primary"
+            onClick={handleSubmit}
+          >
             Submit
           </Button>
         </Modal.Footer>
--- src/components/NewShift/index.js
+++ src/components/NewShift/index.js
@@ -1,31 +1,30 @@
-import React, { useState } from 'react';
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-import './style.css'
-import { validate } from '@babel/types';
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "./style.css";
+import { validate } from "@babel/types";
 
 export default function NewShift(props) {
-
   const [validated, setValidated] = useState(false);
   const [show, setShow] = useState(false);
 
   const [state, setState] = useState({
-    earnings : "",
-    shiftStart : "",
-    shiftEnd : ""
-  })
+    earnings: "",
+    shiftStart: "",
+    shiftEnd: ""
+  });
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const handleChange = e => {
-    const {name, value} = e.target
+    const { name, value } = e.target;
     setState(prevState => ({
       ...prevState,
       [name]: value
-    }))
-  }
+    }));
+  };
 
   const handleSubmit = event => {
     const form = event.currentTarget;
@@ -34,7 +33,7 @@ export default function NewShift(props) {
       event.stopPropagation();
     }
     setValidated(true);
-    if(validated === true){
+    if (validated === true) {
       handleClose();
     }
     console.log(state);
@@ -43,7 +42,27 @@ export default function NewShift(props) {
   return (
     <div>
       <div className="row mb-4">
-        <button onClick={handleShow} type="button" className="add-button mx-auto"><svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="plus" class="svg-inline--fa fa-plus fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M368 224H224V80c0-8.84-7.16-16-16-16h-32c-8.84 0-16 7.16-16 16v144H16c-8.84 0-16 7.16-16 16v32c0 8.84 7.16 16 16 16h144v144c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V288h144c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16z"></path></svg></button>
+        <button
+          onClick={handleShow}
+          type="button"
+          className="add-button mx-auto"
+        >
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="far"
+            data-icon="plus"
+            class="svg-inline--fa fa-plus fa-w-12"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+          >
+            <path
+              fill="currentColor"
+              d="M368 224H224V80c0-8.84-7.16-16-16-16h-32c-8.84 0-16 7.16-16 16v144H16c-8.84 0-16 7.16-16 16v32c0 8.84 7.16 16 16 16h144v144c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V288h144c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16z"
+            ></path>
+          </svg>
+        </button>
       </div>
 
       <Modal show={show} onHide={handleClose} centered>
@@ -54,21 +73,42 @@ export default function NewShift(props) {
           <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Form.Group controlId="validationCustom03">
               <Form.Label>Earnings ($)</Form.Label>
-              <Form.Control name="earnings" onChange={handleChange} value={state.earnings} type="text" placeholder="$55.45" required />
+              <Form.Control
+                name="earnings"
+                onChange={handleChange}
+                value={state.earnings}
+                type="text"
+                placeholder="$0.00"
+                required
+              />
               <Form.Control.Feedback type="invalid">
                 Please provide a valid earnings number
               </Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId="validationCustom04">
               <Form.Label>Shift Start</Form.Label>
-              <Form.Control name="shiftStart" onChange={handleChange} value={state.shiftStart} type="text" placeholder="4:00" required />
+              <Form.Control
+                name="shiftStart"
+                onChange={handleChange}
+                value={state.shiftStart}
+                type="text"
+                placeholder="4:00"
+                required
+              />
               <Form.Control.Feedback type="invalid">
                 Please provide a valid time.
               </Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId="validationCustom05">
               <Form.Label>Shift End</Form.Label>
-              <Form.Control name="shiftEnd" onChange={handleChange} value={state.shiftEnd} type="text" placeholder="8:00" required />
+              <Form.Control
+                name="shiftEnd"
+                onChange={handleChange}
+                value={state.shiftEnd}
+                type="text"
+                placeholder="8:00"
+                required
+              />
               <Form.Control.Feedback type="invalid">
                 Please provide a valid time.
               </Form.Control.Feedback>
@@ -76,9 +116,13 @@ export default function NewShift(props) {
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button className="submit-btn" variant="primary" onClick={handleSubmit}>
+          <Button
+            className="submit-btn"
+            variant="primary"
+            onClick={handleSubmit}
+          >
             Submit
-            </Button>
+          </Button>
         </Modal.Footer>
       </Modal>
     </div>

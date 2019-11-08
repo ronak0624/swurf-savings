b6d22316070f052cfcd805ef9ef9df6d968f992c
Ronak Patel <ronak0624@gmail.com>
2019-11-07 23:11:48 -0800

    signup screen

--- src/components/SignupForm/index.js
+++ src/components/SignupForm/index.js
@@ -1,93 +1,82 @@
-import React from 'react';
-import Form from 'react-boostrap/Form'
-import Button from 'react-boostrap/Button'
+import React, {useState} from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Row from 'react-bootstrap/Row'
 
 export default function SignupForm() {
 
     const [validated, setValidated] = useState(false);
 
+    const [state, setState] = useState({
+        email: "",
+        username: "",
+        password: ""
+    })
+
     const handleSubmit = event => {
         const form = event.currentTarget;
-        if (form.checkValidity() === false) {
+        const isValid = form.checkValidity();
+
+        if (!isValid) {
             event.preventDefault();
             event.stopPropagation();
         }
-
         setValidated(true);
+
+        if(validated){
+            event.preventDefault();
+            console.log(state);
+        }
     };
 
+    const handleChange = e => {
+        const { name, value } = e.target
+        setState(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
     return (
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Form.Row>
-                <Form.Group as={Col} md="4" controlId="validationCustom01">
-                    <Form.Label>First name</Form.Label>
+        <Form className="mt-5 mx-auto w-75 signup-form" noValidate validated={validated} onSubmit={handleSubmit}>
+            <h2 className="display-5 mt-5 text-center"><b>Sign Up</b></h2>
+            <Form.Group md="4" controlId="validationCustomUsername">
+                <Form.Label>Username</Form.Label>
+                <InputGroup>
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                    </InputGroup.Prepend>
                     <Form.Control
-                        required
+                        onChange={handleChange}
+                        name="username"
                         type="text"
-                        placeholder="First name"
-                        defaultValue="Mark"
-                    />
-                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="4" controlId="validationCustom02">
-                    <Form.Label>Last name</Form.Label>
-                    <Form.Control
+                        placeholder="Username"
+                        aria-describedby="inputGroupPrepend"
                         required
-                        type="text"
-                        placeholder="Last name"
-                        defaultValue="Otto"
                     />
-                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-                    <Form.Label>Username</Form.Label>
-                    <InputGroup>
-                        <InputGroup.Prepend>
-                            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <Form.Control
-                            type="text"
-                            placeholder="Username"
-                            aria-describedby="inputGroupPrepend"
-                            required
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            Please choose a username.
+                    <Form.Control.Feedback type="invalid">
+                        Please choose a username.
                     </Form.Control.Feedback>
-                    </InputGroup>
-                </Form.Group>
-            </Form.Row>
-            <Form.Row>
-                <Form.Group as={Col} md="6" controlId="validationCustom03">
-                    <Form.Label>City</Form.Label>
-                    <Form.Control type="text" placeholder="City" required />
-                    <Form.Control.Feedback type="invalid">
-                        Please provide a valid city.
-                </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="3" controlId="validationCustom04">
-                    <Form.Label>State</Form.Label>
-                    <Form.Control type="text" placeholder="State" required />
-                    <Form.Control.Feedback type="invalid">
-                        Please provide a valid state.
-                </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="3" controlId="validationCustom05">
-                    <Form.Label>Zip</Form.Label>
-                    <Form.Control type="text" placeholder="Zip" required />
-                    <Form.Control.Feedback type="invalid">
-                        Please provide a valid zip.
-                </Form.Control.Feedback>
-                </Form.Group>
-            </Form.Row>
-            <Form.Group>
-                <Form.Check
-                    required
-                    label="Agree to terms and conditions"
-                    feedback="You must agree before submitting."
-                />
+                </InputGroup>
             </Form.Group>
-            <Button type="submit">Submit form</Button>
+            <Form.Group controlId="validationEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control name="email" onChange={handleChange} value={state.email} type="text" placeholder="ronak@uw.edu" required />
+                <Form.Control.Feedback type="invalid">
+                    Please provide a valid email.
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="validationPassword1">
+                <Form.Label>Password</Form.Label>
+                <Form.Control name="password" onChange={handleChange} value={state.password} type="password" placeholder="password" required />
+                <Form.Control.Feedback type="invalid">
+                    Please provide a valid password.
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Row>
+                <Button className="mx-auto submit-btn" type="primary" onClick={handleSubmit}>Submit</Button>
+            </Row>
         </Form>
     );
-    }
+}

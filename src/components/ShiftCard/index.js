d19d1b0543594fdc7eff68762d95080d27dcb8b2
Ronak Patel <ronak0624@gmail.com>
2019-11-12 01:20:43 -0800

    cards displaying time

--- src/components/ShiftCard/index.js
+++ src/components/ShiftCard/index.js
@@ -4,12 +4,25 @@ import './style.css'
 import Moment from 'moment';
 
 export default function ShiftCard(props) {
-    const shiftStart = Date.parse(props.shiftStart);
-    console.log(shiftStart, "hours");
+    function calculateHours(start, end){
+        let parsedStart = parseInt(start.substring(0,2));
+        let parsedEnd = parseInt(end.substring(0,2));
+        let prefix = parsedEnd - parsedStart;
+
+        console.log(parsedStart, parsedEnd, "this is the prefix before")
+        if(parsedStart > parsedEnd){
+            prefix = 12 - parsedStart + parsedEnd;
+        }
+        console.log(prefix, "this is the prefix")
+        let suffix = Math.abs(parseInt(end.substring(3,5)) - parseInt(start.substring(3,5)));
+        return prefix + (Number.parseFloat(suffix/60).toPrecision(2))
+    }
+    const hours = calculateHours(props.shiftStart, props.shiftEnd);
+
     return (
         <Card className="mb-5 card-shift mx-auto" bsClass="card-shift">
             <Card.Body>
-                <Card.Title>{shiftStart} hours</Card.Title>
+                <Card.Title>{hours} hours</Card.Title>
                 <Card.Title>${props.earnings} earned</Card.Title>
             </Card.Body>
         </Card>

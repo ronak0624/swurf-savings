84be79271e88ba529cd4ade9ef938e4b01811052
Ronak Patel <ronak0624@gmail.com>
2019-12-27 22:42:21 +0000

    cleaning up unnecessary components/dependencies

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/auth";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -11,6 +14,24 @@ import Shifts from './pages/Shifts/index';
 import Savings from './pages/Savings/index';
 import Register from './pages/Register/index';
 
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+// Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 function App() {
   return (
@@ -19,6 +40,7 @@ function App() {
         <Nav username={sessionStorage.user} />
         <Switch>
           <Route path="/" exact component={Login} />
+          <Route path="/login" exact component={Login} />
           <Route path="/register" exact component={Register} />
           <Route path="/savings" exact component={Savings} />
           <Route path="/shifts" component={Shifts} />

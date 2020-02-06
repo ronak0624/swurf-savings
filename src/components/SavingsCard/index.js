af8d10fc67fd8a6ed5153204deff902b0f6c7519
Ronak Patel <ronak0624@gmail.com>
2020-02-06 15:59:48 -0800

    working active/inactive button for savings

--- src/components/SavingsCard/index.js
+++ src/components/SavingsCard/index.js
@@ -22,9 +22,13 @@ class SavingsCard extends Component {
 
   toggleActive = (id) => {
     API.toggleSavings(id).then(() =>{
-      console.log(this.state.id)
-    })
-    window.location.reload();
+      console.log(this.state.id, this.state.active)
+      this.setState({
+        active: true
+      })
+    }).then(() => {
+      window.location.reload();
+    });
   }
 
   render() {
@@ -35,7 +39,7 @@ class SavingsCard extends Component {
           <div className="d-flex mb-4">
             <div className="mr-auto">
               <div className="d-flex align-items-center mb-3">
-                <div onClick={() => this.toggleActive(this.state.id)} classNameName="task-button">
+                <div onClick={() => this.toggleActive(this.state.id)} className="task-button">
                   {this.props.active ? (
                     <svg className="gi gi-radio-button-on-fill mr-3 fs-sm text-success" width="1em" height="1em" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                       <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8 8 0 0 1-8 8z" /><path d="M12 7a5 5 0 1 0 5 5 5 5 0 0 0-5-5z" />
@@ -72,32 +76,3 @@ class SavingsCard extends Component {
   }
 }
 export default SavingsCard;
-
-
-//// MOCK DATA SYNTHESIZER
-// var savings = [];
-
-// for(var i = 0; i < 40; i++){
-//   let d = new Date();
-//   savings[i] = {
-//           "title": "New Skis",
-//           "price": Math.ceil(Math.random() * 1000),
-//           "priceRemaining": Math.ceil(Math.random() * 400),
-//           "priority": Math.ceil(Math.random() * 3),
-//           "timeCreated": d.getHours(),
-//           "achieved": false,
-//   }
-// }
-
-
-// for(var i = 0; i < 40; i++){
-//   let start = savings[i].price
-//   let end = savings[i].priceRemaining
-
-//   if(start < end){
-//     console.log('swapping');
-//     let swap = savings[i].price;
-//     savings[i].price = savings[i].priceRemaining;
-//     savings[i].priceRemaining = swap;
-//   }
-// }
